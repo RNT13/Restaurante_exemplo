4f1e7ed460be56cb214021750dfcdc5fc768055d
RNT <renatornt13@gmail.com>
2024-10-04 17:56:37 -0300

    commit: inicial

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: 'Seu Telefone'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome.',
+            email: 'Por favor, insira um email válido.',
+        },
+        submitHandler: function(form) {
+            alert('Obrigado pelo contato!');
+            form.reset();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campo(s) incorreto(s)!`);
+            }
+        }
+    });
+});
